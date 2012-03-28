e7200e476d4fe7664146d3e7e9ff7f83199d24b1
mintbridge <paul.dixon@mintbridge.co.uk>
2012-03-28 17:12:19 +0100

    adding a DateTime field

---
+++ classes/Hive/Field/DateTime.php
@@ -0,0 +1,44 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Hive is an intelligent modeling system for Kohana.
+ *
+ * @package    Hive
+ * @category   Field
+ * @author     Woody Gilk <woody@wingsc.com>
+ * @copyright  (c) 2010 Woody Gilk
+ * @license    MIT
+ */
+class Hive_Field_DateTime extends Hive_Field_String {
+
+	/**
+	 * @var  boolean  set the value to "now" on creation?
+	 */
+	public $auto_now_create = FALSE;
+
+	/**
+	 * @var  boolean  set the value to "now" on update?
+	 */
+	public $auto_now_update = FALSE;
+
+	/**
+	 * @var  string  date format for displaying
+	 */
+	public $format = 'd/m/Y H:i';
+
+	public $null = TRUE;
+
+	public function value($value)
+	{
+		if ($value)
+		{
+			$value = date('Y-m-d', strtotime($value));
+		}
+		else
+		{
+			$value = date($this->format, strtotime($value));
+		}
+		
+		return parent::value($value);
+	}
+} // End Hive_Field_DateTime
+
